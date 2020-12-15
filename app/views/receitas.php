3866cc0e4263c8b31bba30765cd15b692ccfc7c3
Renê Nunes Colombaro <rene.colombaro@hotmail.com>
2020-12-14 23:21:02 -0300

    Adicionando resposividade na página receitas e removido alguns comentários redundantes

--- app/views/receitas.php
+++ app/views/receitas.php
@@ -22,6 +22,44 @@
 			</div>
 		</div>
 		<div class='wrapper'>
+			<aside>
+				<h3>Filtros</h3>
+				<div class='filtros'>
+					<form method='GET' action='<?php echo App\Core\Router::getBaseUrl(); ?>receitas'>
+						<span>Nome:</span>
+						<input name='titulo' <?php if(isset($_GET['titulo'])){echo "value='{$_GET['titulo']}'";}?> type='text' placeholder='Nome da receita'>
+						<input type='submit'>
+					</form>
+					<form method='GET' action='<?php echo App\Core\Router::getBaseUrl(); ?>receitas'>
+						<span>Data:</span>
+						<input name='data' <?php if(isset($_GET['data'])){echo "value='{$_GET['data']}'";}?> type='date' placeholder='data de envio'>
+						<input type='submit'>
+					</form>
+					<form method='GET' action='<?php echo App\Core\Router::getBaseUrl(); ?>receitas'>
+						<span>Classificação:</span>
+						<span class="stars">
+							<div class="stars-container">
+								<span class="ativos" style="width:0%">
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+									<i class="fa fa-star" aria-hidden="true"></i>
+								</span>
+								<span class="inativos">
+									<i class="fa fa-star-o" aria-hidden="true"></i>
+									<i class="fa fa-star-o" aria-hidden="true"></i>
+									<i class="fa fa-star-o" aria-hidden="true"></i>
+									<i class="fa fa-star-o" aria-hidden="true"></i>
+									<i class="fa fa-star-o" aria-hidden="true"></i>
+								</span>
+							</div>
+						</span>
+						<input type="range" name="estrelas" min="0" max="100">
+						<input type='submit'>
+					</form>
+				</div>
+			</aside>
 			<main class='ultimos'>
 				<?php
 				if(isset($_GET['titulo']) && !empty($_GET['titulo']))
@@ -74,238 +112,8 @@
 							echo "</a>";
 						}
 					?>
-					<!--<div class='receita'>
-						<div class='imagem'>
-							<img src='<?php echo App\Core\Router::getBaseUrl(); ?>/img/bolo.jpg'>
-						</div>
-						<div class='text'>
-							<h5>Receita 1</h5>
-							<span class="stars">
-								<div class="stars-container">
-									<span class="ativos" style="width:55%">
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-									</span>
-									<span class="inativos">
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-									</span>
-								</div>
-							</span>
-							<div class='footer'>
-								<span>Por: <mark>Bananas</mark></span>
-								<span>13/03/1998</span>
-							</div>
-						</div>
-					</div>
-					<div class='receita'>
-						<div class='imagem'>
-							<img src='<?php echo App\Core\Router::getBaseUrl(); ?>/img/bolo.jpg'>
-						</div>
-						<div class='text'>
-							<h5>Receita 1</h5>
-							<span class="stars">
-								<div class="stars-container">
-									<span class="ativos" style="width:55%">
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-									</span>
-									<span class="inativos">
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-									</span>
-								</div>
-							</span>
-							<div class='footer'>
-								<span>Por: <mark>Bananas</mark></span>
-								<span>13/03/1998</span>
-							</div>
-						</div>
-					</div>
-					<div class='receita'>
-						<div class='imagem'>
-							<img src='<?php echo App\Core\Router::getBaseUrl(); ?>/img/bolo.jpg'>
-						</div>
-						<div class='text'>
-							<h5>Receita 1</h5>
-							<span class="stars">
-								<div class="stars-container">
-									<span class="ativos" style="width:55%">
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-									</span>
-									<span class="inativos">
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-									</span>
-								</div>
-							</span>
-							<div class='footer'>
-								<span>Por: <mark>Bananas</mark></span>
-								<span>13/03/1998</span>
-							</div>
-						</div>
-					</div>
-					<div class='receita'>
-						<div class='imagem'>
-							<img src='<?php echo App\Core\Router::getBaseUrl(); ?>/img/bolo.jpg'>
-						</div>
-						<div class='text'>
-							<h5>Receita 1</h5>
-							<span class="stars">
-								<div class="stars-container">
-									<span class="ativos" style="width:32%">
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-									</span>
-									<span class="inativos">
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-									</span>
-								</div>
-							</span>
-							<div class='footer'>
-								<span>Por: <mark>Bananas</mark></span>
-								<span>13/03/1998</span>
-							</div>
-						</div>
-					</div>
-					<div class='receita'>
-						<div class='imagem'>
-							<img src='<?php echo App\Core\Router::getBaseUrl(); ?>/img/bolo.jpg'>
-						</div>
-						<div class='text'>
-							<h5>Receita 1</h5>
-							<span class="stars">
-								<div class="stars-container">
-									<span class="ativos" style="width:12%">
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-									</span>
-									<span class="inativos">
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-									</span>
-								</div>
-							</span>
-							<div class='footer'>
-								<span>Por: <mark>Bananas</mark></span>
-								<span>13/03/1998</span>
-							</div>
-						</div>
-					</div>
-					<div class='receita'>
-						<div class='imagem'>
-							<img src='<?php echo App\Core\Router::getBaseUrl(); ?>/img/bolo.jpg'>
-						</div>
-						<div class='text'>
-							<h5>Receita 1</h5>
-							<span class="stars">
-								<div class="stars-container">
-									<span class="ativos" style="width:88%">
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-										<i class="fa fa-star" aria-hidden="true"></i>
-									</span>
-									<span class="inativos">
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-										<i class="fa fa-star-o" aria-hidden="true"></i>
-									</span>
-								</div>
-							</span>
-							<div class='footer'>
-								<span>Por: <mark>Bananas</mark></span>
-								<span>13/03/1998</span>
-							</div>
-						</div>
-					</div>-->
 				</div>
 			</main>
-			<aside>
-				<h3>Filtros</h3>
-				<div class='filtros'>
-					<form method='GET' action='<?php echo App\Core\Router::getBaseUrl(); ?>receitas'>
-						<span>Nome:</span>
-						<input name='titulo' <?php if(isset($_GET['titulo'])){echo "value='{$_GET['titulo']}'";}?> type='text' placeholder='Nome da receita'>
-						<input type='submit'>
-					</form>
-					<!--<form method='GET' action='<?php echo App\Core\Router::getBaseUrl(); ?>receitas'>
-						<span>Ingrediente:</span>
-						<input name='ingredinte' type='text' placeholder='Nome de um ingrediente'>
-						<input type='submit'>
-					</form>-->
-					<form method='GET' action='<?php echo App\Core\Router::getBaseUrl(); ?>receitas'>
-						<span>Data:</span>
-						<input name='data' <?php if(isset($_GET['data'])){echo "value='{$_GET['data']}'";}?> type='date' placeholder='data de envio'>
-						<input type='submit'>
-					</form>
-					<form method='GET' action='<?php echo App\Core\Router::getBaseUrl(); ?>receitas'>
-						<span>Classificação:</span>
-						<span class="stars">
-							<div class="stars-container">
-								<span class="ativos" style="width:0%">
-									<i class="fa fa-star" aria-hidden="true"></i>
-									<i class="fa fa-star" aria-hidden="true"></i>
-									<i class="fa fa-star" aria-hidden="true"></i>
-									<i class="fa fa-star" aria-hidden="true"></i>
-									<i class="fa fa-star" aria-hidden="true"></i>
-								</span>
-								<span class="inativos">
-									<i class="fa fa-star-o" aria-hidden="true"></i>
-									<i class="fa fa-star-o" aria-hidden="true"></i>
-									<i class="fa fa-star-o" aria-hidden="true"></i>
-									<i class="fa fa-star-o" aria-hidden="true"></i>
-									<i class="fa fa-star-o" aria-hidden="true"></i>
-								</span>
-							</div>
-						</span>
-						<input type="range" name="estrelas" min="0" max="100">
-						<!--<div class="stars">
-							<span>☆</span>
-							<span>☆</span>
-							<span>☆</span>
-							<span>☆</span>
-							<span>☆</span>
-						</div>-->
-						<input type='submit'>
-					</form>
-				</div>
-			</aside>
 		</div><!-- Wrapper -->
 		<div class='pages'>
 			<?php
@@ -322,7 +130,6 @@
 				}
 			?>
 		</div>
-		
 		<?php include_once('../app/views/footer.php');?>
 	</body>
 </html>
