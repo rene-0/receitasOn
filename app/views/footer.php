2c9ac83070f5d64c761be3e98adf6e1018a3b0a6
Renê Nunes Colombaro <rene.colombaro@hotmail.com>
2020-12-14 22:30:47 -0300

    Adicionando responsividade em home e pequena mudança em footer

--- app/views/footer.php
+++ app/views/footer.php
@@ -1,6 +1,6 @@
 <footer>
 	<div id='toast'></div>
 	<p>rene.colombaro@hotmail.com</p>
-	<p>Site exclusivamente para portfólio</p>
+	<p>Site exclusivo para portfólio</p>
 	<p>Data: 27/09/2020</p>
 </footer>
