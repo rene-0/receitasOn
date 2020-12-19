22d44fb3b37156680d31358e77056076e4924a33
Renê Nunes Colombaro <rene.colombaro@hotmail.com>
2020-12-18 21:51:36 -0300

    Corrigindo o erro do menu de navegação

--- public/js/main.js
+++ public/js/main.js
@@ -11,14 +11,12 @@ $(document).ready(function (){
 	$('#btn-mobile').click(function (){
 		if($('#nav-cortina').css('display') == 'none')
 		{
-			console.log('Teste open');
 			$('#nav-cortina').fadeIn();
 		}
 	});
 	$('#mobile-close').click(function (){
 		if($('#nav-cortina').css('display') == 'block')
 		{
-			console.log('Teste close');
 			$('#nav-cortina').fadeOut();
 		}
 	});
